a8ff578a0f846d0d220e7254a16cb51fdfca94a7
Leif Eirik Lislegård <lislei@statkart.no>
2012-01-25 08:35:37 +0100

    SKIF-106: enkelt gradle byggesystem
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 38970]

---
+++ setEnv_personal - template.cmd
@@ -0,0 +1,20 @@
+@rem
+@rem SETTER BRUKER/MASKIN SPESEFIKKE ENVIRONMENT VARIABLE FOR HOVEDPROSJEKTET
+@rem
+
+@rem Java, Gradle
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_21
+set GRADLE_HOME=C:\bin\gradle-1.0-milestone-4
+
+@rem Groovy (optional)
+set GROOVY_HOME=C:\bin\groovy-1.7.10
+
+
+@rem Definer lokal nexus repo for offline utvikling (optional)
+@rem set REPO_PUBLIC=http://localhost:8081/nexus/content/repositories/statkart/
+
+@rem Setup Perforce windows integrasjon (optional)
+p4 set P4USER=%USERNAME%
+p4 set P4CLIENT=%P4USER%-%COMPUTERNAME%
+p4 set P4PORT=skrivap42.statkart.no:1666
+
