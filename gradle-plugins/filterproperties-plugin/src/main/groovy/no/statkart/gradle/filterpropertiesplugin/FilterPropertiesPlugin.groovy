4bb7e1f95ec7016c440fbf65c75476ffe45ffdc5
Leif Eirik Lislegård <lislei@statkart.no>
2012-02-18 03:28:44 +0100

    SKIF-142: flyttet ut filterproperties-plugin
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 39539]

---
+++ gradle-plugins/filterproperties-plugin/src/main/groovy/no/statkart/gradle/filterpropertiesplugin/FilterPropertiesPlugin.groovy
@@ -0,0 +1,68 @@
+package no.statkart.gradle.filterpropertiesplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.Project
+import java.util.Map.Entry
+import no.statkart.gradle.util.GradleUtil
+import org.apache.tools.ant.filters.ReplaceTokens
+
+/**
+ * @author Thor Åge Eldby
+ */
+class FilterPropertiesPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.getPlugins().apply(JavaPlugin.class);
+        FilterPropertiesConvention convention = new FilterPropertiesConvention()
+        project.getConvention().getPlugins().put('statKartFilterProperties', convention)
+        project.afterEvaluate {
+            if (it.state.failure == null) {
+                createFilterTasks(convention, project)
+            }
+        }
+    }
+
+    private def createFilterTasks(FilterPropertiesConvention convention, Project project) {
+        convention.resources.each { Entry<String, String> entry ->
+            String scope = entry.key
+            String path = entry.value
+            def unfilteredPropertiesDir = project.file(path)
+            if (unfilteredPropertiesDir.isDirectory()) {
+                def filteredResourcesDir = project.file("build/generated/${scope}/resources")
+                String taskName = "filterResources$scope"
+                project.task (taskName) {
+                    inputs.dir(unfilteredPropertiesDir)
+                    inputs.properties(convention.properties)
+                    outputs.dir(filteredResourcesDir)
+                    doLast {
+                        // Ant copy forstår ikke at properties har endret seg; derfor må vi tvinge refilterering
+                        project.delete(filteredResourcesDir);
+                        project.copy {
+                            from(unfilteredPropertiesDir)
+                            into(filteredResourcesDir)
+                            filter(ReplaceTokens, tokens: convention.properties)
+                        }
+                        project.ant.touch() { fileset(dir: filteredResourcesDir)}
+                    }
+                }
+                project.sourceSets."$scope".resources.srcDirs -= unfilteredPropertiesDir
+                project.sourceSets."$scope".resources.srcDir filteredResourcesDir
+                project.processResources.dependsOn taskName
+                GradleUtil.makeIdeaShowBuildDirectory(project)
+            }
+        }
+    }
+
+}
+
+class FilterPropertiesConvention {
+    Map<String, String> resources = [main: 'src/main/unfilteredResources', test: 'src/test/unfilteredResources']
+    Map<String, String> properties = Collections.emptyMap()
+
+    def statKartFilterProperties(Closure closure) {
+        closure.delegate = this
+        closure()
+    }
+}
