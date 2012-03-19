a47add3ca5714ed08c68cb014331e00aefbffd62
Leif Eirik Lislegård <lislei@statkart.no>
2012-03-19 08:32:08 +0100

    SKIF-142: refaktor XjcPlugin
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 40146]

--- gradle-plugins/xjc-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/xjc/XjcPlugin.groovy
+++ gradle-plugins/xjc-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/xjc/XjcPlugin.groovy
@@ -1,43 +1,132 @@
 package no.statkart.sktools.gradle.plugins.xjc
 
-import no.statkart.sktools.gradle.plugins.xjc.util.GradleUtil
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.artifacts.ConfigurationContainer
 import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.tasks.Directory
+import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.SourceTask
+import org.gradle.api.tasks.ConventionValue
+import org.gradle.api.plugins.Convention
+import org.gradle.api.internal.IConventionAware
 
 /**
- * todo: dokumentasjon
+ * Genererer JAXB java klasser basert på <code>*.xsd<code> filer. <br />
+ * Pluginen baserer seg på {@code JavaPlugin} og integrerer seg dynamiskt med denne.
  *
+ * Se dokumentasjon for <i>xjc-plugins<i> modul for bruk av evt utvidelser.
+ * <ul>
+ *  <li>{@link GrunnbokDocPlugin}
+ *  <li>{@link ListGenPlugin}
+ * </ul>
  *
  * @since 1.0
  * @author Thor Åge Eldby
+ * @author Leif Lislegård
+
  */
 class XjcPlugin implements Plugin<Project> {
 
+    final static String CONVENTION_NAME = 'xjc'
     final static String JAXB_CONFIGURATION_NAME = 'jaxb'
     final static String XJC_TASK_NAME = 'xjcGenerate'
 
 
     @Override
     void apply(Project project) {
-        XjcPluginConvention convention = new XjcPluginConvention(project)
-        project.convention.plugins.xjc = convention
-        project.getPlugins().apply(JavaPlugin.class);
-        Task generateTask = project.task(XJC_TASK_NAME, type: XjcTask)
-        Task compileJavaTask = project.getTasks().getByName(JavaPlugin.COMPILE_JAVA_TASK_NAME)
-        compileJavaTask.dependsOn(generateTask)
-        GradleUtil.makeIdeaShowBuildDirectory(project)
-        ConfigurationContainer configurations = project.getConfigurations();
-        Configuration moduleConfiguration = configurations.add(JAXB_CONFIGURATION_NAME).setVisible(false)
-                .setDescription("Classpath for jaxb library and extensions.");
-        generateTask.dependsOn(moduleConfiguration)
+        project.apply plugin: JavaPlugin.class
 
-        Task xjcCreateJavaDir = project.task('xjcCreateJavaDir', type: Directory) { dir = convention.targetDirectory }
-        Task ideaTask = project.getTasks().getByName('ideaModule')
-        ideaTask.dependsOn(xjcCreateJavaDir)
+        final XjcConvention xjcConvention = new XjcConvention(project)
+        project.convention.plugins.put(XjcPlugin.CONVENTION_NAME, xjcConvention);
+
+        final Configuration xjcConfiguration = createConfiguration(project);
+
+        //creates the task
+        Task xjcTask = project.task(XJC_TASK_NAME, type: XjcTask.class).dependsOn(
+                xjcConfiguration,
+        );
+
+        project.tasks[JavaPlugin.PROCESS_RESOURCES_TASK_NAME].dependsOn(
+                XjcPlugin.XJC_TASK_NAME,    //hooks it into prior to 'resources' task
+        );
+
+
+
+        //setting defaults if not already configured
+        project.afterEvaluate {
+            setConventionalDefaults(xjcConvention)
+            configureSourceSet(xjcConvention)
+            configureConventionalValuesForXjcTask(project, xjcConvention)
+        }
+
     }
+
+    /**
+     * Legger til mappe for generert kildekode til valgt source sett.
+     * <p>
+     * Se {@link XjcConvention#sourceSetName} for valg av sourceSet
+     */
+    private void configureSourceSet(XjcConvention xjcConvention) {
+        Project project = xjcConvention.project
+        JavaPluginConvention javaConvention = project.getConvention().getPlugins().get("java");
+        SourceSet sourceSet = javaConvention.getSourceSets().getByName(xjcConvention.sourceSetName)
+
+        //legger genererte java filer til source set
+        sourceSet.getJava().srcDir(xjcConvention.targetDir)
+
+        //legger til source slik at de kan bli plukket opp av dokumentajonsverktøy, kildekode distribusjon mm
+        SourceTask xjcTask = project.getTasks().getByName(XjcPlugin.XJC_TASK_NAME)
+        sourceSet.getAllSource().srcDir(xjcTask)
+
+    }
+
+    /**
+     * Setter default verdier etter at all annen konfigurasjon er gjort.
+     */
+    private void setConventionalDefaults(XjcConvention xjcConvention) {
+        Project project = xjcConvention.project
+
+        if (xjcConvention.targetDir == null) {
+            xjcConvention.targetDir = project.file("gen/${xjcConvention.sourceSetName}/java")
+        }
+
+        xjcConvention.schema.each {
+            if (it.includes == null) it.includes = ['*.xsd']
+        }
+    }
+
+
+    private configureConventionalValuesForXjcTask(final Project project, final XjcConvention xjcConvention) {
+        project.tasks.getByName(XJC_TASK_NAME).getConventionMapping().with {
+            map("defaultSource", new ConventionValue() { //tildeler en ikke dynamisk default verdi
+                public Object getValue(Convention conventionManager, IConventionAware conventionAwareObject) {
+                    return project.files(xjcConvention.schema.collect {it.dir}).getAsFileTree();  //default source
+                }
+            });
+            map("schemas", new ConventionValue() {
+                public Object getValue(Convention conventionManager, IConventionAware conventionAwareObject) {
+                    return xjcConvention.schema;
+                }
+            });
+            map("outputDirectory", new ConventionValue() {
+                public Object getValue(Convention conventionManager, IConventionAware conventionAwareObject) {
+                    return xjcConvention.targetDir;
+                }
+            });
+            map("classpath", new ConventionValue() {
+                public Object getValue(Convention conventionManager, IConventionAware conventionAwareObject) {
+                    return project.getConfigurations().getByName(XjcPlugin.JAXB_CONFIGURATION_NAME);
+                }
+            });
+        }
+    }
+
+    //classpath classpath for jaxb libs (needed at runtime)
+
+    private Configuration createConfiguration(Project project) {
+        return project.configurations.add(JAXB_CONFIGURATION_NAME).setVisible(false).setDescription("Classpath for jaxb library and extensions.");
+    }
+
 }
--- gradle-plugins/xjc-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/xjc/XjcTask.groovy
+++ gradle-plugins/xjc-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/xjc/XjcTask.groovy
@@ -1,53 +1,77 @@
 package no.statkart.sktools.gradle.plugins.xjc
 
-import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
-import org.gradle.api.Project
+
 import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
 
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputDirectory
+
 /**
- * todo: dokumentasjon
+ * Eksekverer XJC task via ant.
+ * Kobler inn evt plugin funksjonalitet i hht konfigurasjon av convention. Se {@link no.statkart.sktools.gradle.plugins.xjc.model.XjcOptions } for detaljer.
+ *
+ * <p>
+ * Følgende plugin funksjonalitet er implementert:
+ * <ul>
+ *     <li>com.sun.tools.xjc.addon.statkart.GrunnbokDocPlugin
+ *     <li>com.sun.tools.xjc.addon.statkart.ListGenPluginTest
+ * </ul>
+ *
+ *
+ * Funksjonalitet implementeres i :build-utils:xjc-plugins modul
  *
  *
  * @since 1.0
  * @author Thor Åge Eldby
  * @author Leif Lislegård
  */
-class XjcTask extends DefaultTask {
-    XjcPluginConvention convention = getProject().convention.plugins.xjc
-    List schemas = convention.schemas
-    String targetDirectory = convention.targetDirectory
+class XjcTask extends SourceTask {
+
+    @Input
+    List<Schema> schemas;
+
+    @OutputDirectory
+    File outputDirectory;
+
+    @Input
+    FileCollection classpath;
+
 
     XjcTask() {
-        Project project = getProject()
-        project.afterEvaluate {
-            schemas.each { s -> inputs.dir(s.dir) }
-            if (!project.sourceSets.main.java.srcDirs.contains(targetDirectory)) {
-                project.sourceSets.main.java.srcDirs += targetDirectory
-            }
-            outputs.dir(targetDirectory)
-        }
+
     }
+
 
     @TaskAction
     def generate() {
-        if (schemas.isEmpty()) {
-            throw new GradleException("No schemas defined for xjc generation: statKartXjc.schema(dir, includes)")
-        }
-        FileCollection libraries = project.configurations[XjcPlugin.JAXB_CONFIGURATION_NAME]
-        ant.taskdef(name: 'xjc', classname: 'com.sun.tools.xjc.XJCTask', classpath: libraries.asPath)
-        ant.mkdir(dir: targetDirectory)
-        schemas.each { s ->
-            ant.xjc(destDir: targetDirectory, extension: s.withGrunnbokDoc != null) {
-                if (s.withGrunnbokDoc) {
-                    arg(line: "-grunnbokDoc $s.withGrunnbokDoc")
+        getOutputDirectory().mkdirs()
+
+        ant.taskdef(name: 'xjc', classname: 'com.sun.tools.xjc.XJCTask', classpath: getClasspath().getAsPath())
+
+        getSchemas().each { Schema s ->
+            def antTask = ant.xjc(destDir: getOutputDirectory(), extension: !s.xjcOptions.isEmpty()) {
+                if (s.xjcOptions.containsKey(Schema.GRUNNBOK_DOC)) {
+                    Map params = s.xjcOptions.get(Schema.GRUNNBOK_DOC)
+                    def args = params.values().join(' ')
+                    arg(line: "-grunnbokDoc ${args}")
                 }
-                if (s.withListAdapter) {
-                    arg(line: '-listgen')
+                if (s.xjcOptions.containsKey(Schema.LIST_ADAPTER)) {
+                    Map params = s.xjcOptions.get(Schema.LIST_ADAPTER)
+                    def args = params.entrySet().collect {"${it.key}=${it.value}"}.join(' ')
+                    arg(line: "-listgen ${args}")
                 }
-                schema(dir: s.dir, includes: s.includes)
+                schema(dir: s.dir) {
+                    s.includes.each {
+                        include(name:it)
+                    }
+                }
             }
+
+           antTaks = null; //debug point
         }
+
     }
 }
