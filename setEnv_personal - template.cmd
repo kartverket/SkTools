710dec4187d525683c5c94280dd6c0339910fab5
Leif Eirik Lislegård <lislei@statkart.no>
2012-11-02 12:58:02 +0100

    ny adresse for nexus
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 43746]

--- setEnv_personal - template.cmd
+++ setEnv_personal - template.cmd
@@ -14,18 +14,18 @@ rem set GROOVY_HOME=C:\bin\groovy-1.8.6
 set ORG_GRADLE_PROJECT_WEBLOGIC_HOME=C:\bea_wls10.3.5
 
 @rem echo Definerer Maven Repository for utvikling [optional]
-@rem set ORG_GRADLE_PROJECT_MAVEN_REPO=http://skrivap92.statkart.no:8001/nexus/content/groups/public/
+@rem set ORG_GRADLE_PROJECT_MAVEN_REPO=http://nexus.statkart.no:8090/nexus/content/groups/public/
 
 @rem echo Definerer lokalt Nexus Maven Reposotory repo for offline utvikling [optional]
 @rem set ORG_GRADLE_PROJECT_MAVEN_REPO=http://localhost:8081/nexus/content/repositories/statkart/
 
 @rem echo Definerer Maven Repository for releases [optional]
-@rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_RELEASES=http://skrivap92.statkart.no:8001/nexus/content/repositories/releases/
+@rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_RELEASES=http://nexus.statkart.no:8090/nexus/content/repositories/releases/
 @rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_RELEASES_USERNAME=admin
 @rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_RELEASES_PASSWORD=admin123
 
 @rem echo Definerer Maven Repository for snapshots [optional]
-@rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_SNAPSHOTS=http://skrivap92.statkart.no:8001/nexus/content/repositories/snapshots/
+@rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_SNAPSHOTS=http://nexus.statkart.no:8090/nexus/content/repositories/snapshots/
 @rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_SNAPSHOTS_USERNAME=admin
 @rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_SNAPSHOTS_PASSWORD=admin123
 
@@ -33,5 +33,5 @@ set ORG_GRADLE_PROJECT_WEBLOGIC_HOME=C:\bea_wls10.3.5
 @rem Setup Perforce windows integrasjon (optional)
 p4 set P4USER=%USERNAME%
 p4 set P4CLIENT=%P4USER%-%COMPUTERNAME%
-p4 set P4PORT=skrivap42.statkart.no:1666
+p4 set P4PORT=perforce.statkart.no:1666
 
