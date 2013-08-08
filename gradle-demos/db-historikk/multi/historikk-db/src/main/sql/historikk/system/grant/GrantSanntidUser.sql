ea53732d7e9cd09842fbdb41d5f95d5ba329208c
Leif Eirik Lislegård <lislei@statkart.no>
2013-08-08 14:14:16 +0100

    GBOK-1871: grants og synonymer for sanntidsbruker slik at denne kan se generert historikk + aksessere HISTORIKK_TRANSACTION.
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 49346]

--- gradle-demos/db-historikk/multi/historikk-db/src/main/sql/historikk/system/grant/GrantSanntidUser.sql
+++ gradle-demos/db-historikk/multi/historikk-db/src/main/sql/historikk/system/grant/GrantSanntidUser.sql
@@ -6,5 +6,21 @@ PROMPT Granting historikk access for @sanntid_db_username@...
 
 GRANT EXECUTE ON "@historikk_db_schema@".SNAPSHOT_TIME TO "@sanntid_db_username@";
 
--- Kan velge om sanntidsbruker skal ha tilgang til aa modifisere paa transaksjonsnivaa
---GRANT EXECUTE ON "@historikk_db_schema@".HISTORIKK_TRANSACTION TO "@sanntid_db_username@";
+-- Kan velge om sanntidsbruker skal ha tilgang til aa modifisere paa transaksjonsnivaa  (slått på for denne demoen)
+GRANT EXECUTE ON "@historikk_db_schema@".HISTORIKK_TRANSACTION TO "@sanntid_db_username@";
+
+-- Kan velge om sanntidsbruker skal kunne leSE historikk-data eller ikke (slått på for denne demoen)
+BEGIN
+  FOR i IN (SELECT object_name FROM all_objects where owner = '@historikk_db_username@' AND object_type = 'VIEW') LOOP
+    EXECUTE IMMEDIATE('GRANT SELECT ON @historikk_db_schema@.' || i.object_name || ' TO @sanntid_db_username@');
+  END LOOP;
+  COMMIT;
+END;
+/
+BEGIN
+  FOR i IN (SELECT object_name FROM all_objects where owner = '@historikk_db_username@' AND object_type = 'TABLE') LOOP
+    EXECUTE IMMEDIATE('GRANT SELECT ON @historikk_db_schema@.' || i.object_name || ' TO @sanntid_db_username@');
+  END LOOP;
+  COMMIT;
+END;
+/
---
+++ gradle-demos/db-historikk/multi/historikk-db/src/main/sql/historikk/system/synonym/SynonymsForSanntidUser.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE SYNONYM "@sanntid_db_schema@".SNAPSHOT_TIME FOR "@historikk_db_schema@".SNAPSHOT_TIME;
+
+-- må vurdere om sanntidsbruker skal trenge å være i stand til å sette transaksjonstidspunkt eller ikke... trolig ikke. (slått på for denne demoen)
+CREATE OR REPLACE SYNONYM "@sanntid_db_schema@".HISTORIKK_TRANSACTION FOR "@historikk_db_schema@".HISTORIKK_TRANSACTION;
+
+-- velger at bruker for sanntids-db kan se historikktabell (slått på for denne demoen)
+BEGIN
+  FOR i IN (SELECT object_name FROM all_objects where owner = '@historikk_db_username@' AND object_type = 'TABLE' AND object_name like '%\_H' escape '\') LOOP
+    EXECUTE IMMEDIATE('CREATE OR REPLACE SYNONYM "@sanntid_db_schema@".' || i.object_name || ' FOR @historikk_db_schema@.' || i.object_name );
+  END LOOP;
+  COMMIT;
+END;
+/
