2c68ccb663d3ac1823f47e0bacfcd42fc50d31e2
Leif Eirik Lislegård <lislei@statkart.no>
2013-08-09 14:07:19 +0100

    fnuttering av objektnavn
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 49391]

--- gradle-demos/db-historikk/multi/historikk-db/src/main/sql/demo/historikk/schema/PurgeHistorikkPackages.sql
+++ gradle-demos/db-historikk/multi/historikk-db/src/main/sql/demo/historikk/schema/PurgeHistorikkPackages.sql
@@ -2,7 +2,7 @@ PROMPT purging packages for user @historikk_db_username@
 BEGIN
   FOR i IN (SELECT object_name FROM all_objects where owner = '@historikk_db_username@' AND object_type = 'PACKAGE')
     LOOP
-        EXECUTE IMMEDIATE('DROP PACKAGE @historikk_db_username@.' || i.object_name);
+        EXECUTE IMMEDIATE('DROP PACKAGE "@historikk_db_username@"."' || i.object_name || '"');
     END LOOP;
 END;
 /
