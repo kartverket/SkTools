8ed8a42d86bd81cdec2c48b6ae83c0549ee21657
Leif Eirik Lislegård <lislei@statkart.no>
2013-08-19 09:56:42 +0100

    GBOK-1912: innført mulighet for velging av weblogic versjoner via env variable. Disse blir bygget inn i idea prosjektet.
    Tatt vekk mulighet for uploading av snapshot-versjoner til maven repo da dette ikke skal gjøres (policy)
    
    Forbedret setEnv script
    
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 49572]

--- setEnv_personal - template.cmd
+++ setEnv_personal - template.cmd
@@ -1,40 +1,51 @@
 @rem
-@rem SETTER BRUKER/MASKIN SPESEFIKKE ENVIRONMENT VARIABLE FOR HOVEDPROSJEKTET
+@rem SETTER BRUKER/MASKIN SPESEFIKE ENVIRONMENT VARIABLE FOR HOVEDPROSJEKTET
 @rem
 
-@rem Java, Gradle
+
+@rem *******************************************************************************************************************
+@rem * Gradle & Project Setup
+@rem *******************************************************************************************************************
 set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_21
-set GRADLE_HOME=C:\bin\gradle\gradle-1.0
 set GRADLE_HOME=C:\bin\gradle\gradle-1.2
 set GRADLE_HOME=C:\bin\gradle\gradle-1.5
-
-@rem Groovy (optional)
-rem set GROOVY_HOME=C:\bin\groovy-1.8.6
-
-@rem bestemmer weblogic home
-set ORG_GRADLE_PROJECT_WEBLOGIC_HOME=C:\bea_wls10.3.5
+set GRADLE_HOME=C:\bin\gradle\gradle-1.6
 
 @rem bestemmer versjon av prosjektet [optional - må benyttes på ikke numeriske kodebrancher ala trunk]
 @rem set ORG_GRADLE_PROJECT_sktools_versjon=1.2-SNAPSHOT
 
 
+@rem *******************************************************************************************************************
+@rem * Weblogic Setup
+@rem *******************************************************************************************************************
+set WEBLOGIC_HOME=C:\bin\bea1035
+set WEBLOGIC_VERSION=10.3.5
+@rem LOCALHOSTNAME bestemmer gyldig hostname for bruk med sertifikater (default til %COMPUTERNAME%.statkart.no)
+@rem set LOCALHOSTNAME=%COMPUTERNAME%.statkart.no
 
-@rem echo Definerer lokalt Nexus Maven Reposotory repo for offline utvikling [optional]
-@rem set ORG_GRADLE_PROJECT_MAVEN_REPO=http://localhost:8081/nexus/content/repositories/statkart/
 
-@rem echo Definerer Maven Repository for releases [optional]
-@rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_RELEASES=http://localhost:8081/nexus/content/repositories/releases/
+@rem *******************************************************************************************************************
+@rem * Maven Repo & Nexus Setup  (optional)
+@rem *******************************************************************************************************************
+@rem Definer lokal nexus repo for offline utvikling (optional)
+@rem set MAVEN_REPO=http://localhost:8081/nexus/content/repositories/statkart/
+
+@rem Definerer repo for deploying av bygget applikasjon (nexus)
+@rem set REPO_UPLOAD_RELEASES=http://admin:admin123@nexus.statkart.no:8090/nexus/content/repositories/releases/
 @rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_RELEASES_USERNAME=admin
 @rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_RELEASES_PASSWORD=admin123
 
-@rem echo Definerer Maven Repository for snapshots [optional]
-@rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_SNAPSHOTS=http://localhost:8081/nexus/content/repositories/snapshots/
-@rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_SNAPSHOTS_USERNAME=admin
-@rem set ORG_GRADLE_PROJECT_REPO_UPLOAD_SNAPSHOTS_PASSWORD=admin123
+
+@rem *******************************************************************************************************************
+@rem * Groovy Setup (optional)
+@rem *******************************************************************************************************************
+@rem set GROOVY_HOME=C:\bin\groovy-1.8.6
 
 
-@rem Setup Perforce windows integrasjon (optional)
-p4 set P4USER=%USERNAME%
-p4 set P4CLIENT=%P4USER%-%COMPUTERNAME%
-p4 set P4PORT=perforce.statkart.no:1666
+@rem *******************************************************************************************************************
+@rem * Perforce for Windows integrasjon (optional)
+@rem *******************************************************************************************************************
+@rem p4 set P4CLIENT=%USERNAME%-%COMPUTERNAME%
+@rem p4 set P4PORT=perforce.statkart.no:1666
+@rem p4 set P4USER=%USERNAME%
 
