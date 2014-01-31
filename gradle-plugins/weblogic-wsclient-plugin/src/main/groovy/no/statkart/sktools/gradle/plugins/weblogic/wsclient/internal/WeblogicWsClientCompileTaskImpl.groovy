5f55152a8f4e9f86eb9f0d12c80c5148502e57d3
Tor Egil Strand <strant@statkart.no>
2014-01-31 15:44:28 +0100

    SKTOOLS-92: Prøver å få kompatibilitet med nyere Gradle-versjoner, på bekostning av Gradle 1.1 og eldre
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 54151]

--- gradle-plugins/weblogic-wsclient-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/wsclient/internal/WeblogicWsClientCompileTaskImpl.groovy
+++ gradle-plugins/weblogic-wsclient-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/wsclient/internal/WeblogicWsClientCompileTaskImpl.groovy
@@ -1,93 +1,15 @@
 package no.statkart.sktools.gradle.plugins.weblogic.wsclient.internal
 
-import org.gradle.api.tasks.compile.AbstractCompile
+import org.gradle.api.tasks.compile.JavaCompile
 import no.statkart.sktools.gradle.plugins.weblogic.wsclient.WeblogicWsClientCompileTask
-import org.gradle.api.internal.tasks.compile.JavaCompileSpec
-import org.gradle.api.tasks.compile.CompileOptions
-import org.gradle.api.AntBuilder
-import org.gradle.internal.Factory
-import org.gradle.api.internal.tasks.compile.JavaCompilerFactory
-import org.gradle.api.internal.tasks.compile.InProcessJavaCompilerFactory
-import org.gradle.api.internal.project.ProjectInternal
-import org.gradle.api.internal.file.TemporaryFileProvider
-import org.gradle.api.internal.tasks.compile.DefaultJavaCompilerFactory
-import org.gradle.api.internal.tasks.compile.DelegatingJavaCompiler
-import org.gradle.api.internal.tasks.compile.IncrementalJavaCompiler
-import org.gradle.api.tasks.TaskAction
-import org.gradle.api.internal.tasks.compile.DefaultJavaCompileSpec
-import org.gradle.api.tasks.WorkResult
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.Nested
-import org.gradle.api.internal.tasks.compile.Compiler
 
 /**
  * Task som kompilerer java kildekoden. Se {@link WeblogicWsClientCompileTask}
  *
- * PS: extender ikke JavaCompile da man ønsker bakoverkompabilitet med gradle.version < 1.2
- *
  * @author Leif Lislegård
  * @since 1.2
  */
-public class WeblogicWsClientCompileTaskImpl extends AbstractCompile implements WeblogicWsClientCompileTask {
-    private org.gradle.api.internal.tasks.compile.Compiler<JavaCompileSpec> javaCompiler;
-    private File dependencyCacheDir;
-    private final CompileOptions compileOptions = new CompileOptions();
-
+public class WeblogicWsClientCompileTaskImpl extends JavaCompile implements WeblogicWsClientCompileTask {
     public WeblogicWsClientCompileTaskImpl() {
-        Factory<AntBuilder> antBuilderFactory = getServices().getFactory(AntBuilder.class);
-        JavaCompilerFactory inProcessCompilerFactory = new InProcessJavaCompilerFactory();
-        ProjectInternal projectInternal = (ProjectInternal) getProject();
-        JavaCompilerFactory defaultCompilerFactory
-        if (projectInternal.gradle.gradleVersion <= '1.6') {
-            TemporaryFileProvider tempFileProvider = projectInternal.getServices().get(TemporaryFileProvider.class);
-            defaultCompilerFactory = new DefaultJavaCompilerFactory(projectInternal, tempFileProvider, antBuilderFactory, inProcessCompilerFactory);
-        } else {
-            defaultCompilerFactory = new DefaultJavaCompilerFactory(projectInternal, antBuilderFactory, inProcessCompilerFactory);
-        }
-        org.gradle.api.internal.tasks.compile.Compiler<JavaCompileSpec> delegatingCompiler = new DelegatingJavaCompiler(defaultCompilerFactory);
-        javaCompiler = new IncrementalJavaCompiler(delegatingCompiler, antBuilderFactory, getOutputs());
-    }
-
-    @TaskAction
-    protected void compile() {
-        DefaultJavaCompileSpec spec = new DefaultJavaCompileSpec();
-        spec.setSource(getSource());
-        spec.setDestinationDir(getDestinationDir());
-        spec.setClasspath(getClasspath());
-        spec.setDependencyCacheDir(getDependencyCacheDir());
-        spec.setSourceCompatibility(getSourceCompatibility());
-        spec.setTargetCompatibility(getTargetCompatibility());
-        if (project.gradle.gradleVersion >= "1.2") {
-            spec.compileOptions = compileOptions;   //bakoverkompabilitet med gradle < 1.2
-        }
-        WorkResult result = javaCompiler.execute(spec);
-        setDidWork(result.getDidWork());
-    }
-
-    @OutputDirectory
-    public File getDependencyCacheDir() {
-        return dependencyCacheDir;
-    }
-
-    public void setDependencyCacheDir(File dependencyCacheDir) {
-        this.dependencyCacheDir = dependencyCacheDir;
-    }
-
-    /**
-     * Returns the compilation options.
-     *
-     * @return The compilation options.
-     */
-    @Nested
-    public CompileOptions getOptions() {
-        return compileOptions;
-    }
-
-    public Compiler<JavaCompileSpec> getJavaCompiler() {
-        return javaCompiler;
-    }
-
-    public void setJavaCompiler(Compiler<JavaCompileSpec> javaCompiler) {
-        this.javaCompiler = javaCompiler;
     }
 }
