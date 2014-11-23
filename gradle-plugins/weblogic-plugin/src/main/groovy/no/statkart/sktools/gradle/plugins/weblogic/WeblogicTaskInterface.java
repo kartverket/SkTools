1b9f9e7a3a956d6226b1406a57cbb591a6fedeeb
Leif Eirik Lislegård <lislei@statkart.no>
2014-11-23 02:46:14 +0100

    java syntax
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 61229]

--- gradle-plugins/weblogic-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/WeblogicTaskInterface.java
+++ gradle-plugins/weblogic-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/WeblogicTaskInterface.java
@@ -1,8 +1,8 @@
-package no.statkart.sktools.gradle.plugins.weblogic
+package no.statkart.sktools.gradle.plugins.weblogic;
 
-import org.gradle.api.file.FileCollection
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.internal.IConventionAware
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.internal.IConventionAware;
 
 /**
  * Interface for {@link org.gradle.api.internal.ConventionTask}-implementasjoner
