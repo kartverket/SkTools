3e5952b837d25e5e953a3f53fbe40bd6628531e9
Leif Eirik Lislegård <lislei@statkart.no>
2014-11-24 14:50:58 +0100

    SKTOOLS-127: java syntax
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 61261]

--- gradle-plugins/weblogic-wswar-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/wswar/WeblogicWsCompileTask.java
+++ gradle-plugins/weblogic-wswar-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/wswar/WeblogicWsCompileTask.java
@@ -1,18 +1,23 @@
-package no.statkart.sktools.gradle.plugins.weblogic.wswar
+package no.statkart.sktools.gradle.plugins.weblogic.wswar;
 
-import no.statkart.sktools.gradle.plugins.weblogic.WeblogicTaskInterface
-import no.statkart.sktools.gradle.plugins.weblogic.compile.DefaultWeblogicCompileSpec
-import org.gradle.api.file.FileCollection
-import org.gradle.api.internal.project.ProjectInternal
-import org.gradle.api.logging.LogLevel
-import org.gradle.api.logging.Logger
-import org.gradle.api.logging.Logging
-import org.gradle.api.tasks.Nested
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.WorkResult
-import org.gradle.api.tasks.compile.AbstractCompile
-import org.gradle.api.tasks.compile.CompileOptions
+import no.statkart.sktools.gradle.plugins.weblogic.WeblogicTaskInterface;
+import no.statkart.sktools.gradle.plugins.weblogic.compile.DefaultWeblogicCompileSpec;
+import org.codehaus.groovy.runtime.MethodClosure;
+import org.gradle.api.Project;
+import org.gradle.api.file.CopySpec;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.internal.project.ProjectInternal;
+import org.gradle.api.logging.LogLevel;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+import org.gradle.api.tasks.Nested;
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.WorkResult;
+import org.gradle.api.tasks.compile.AbstractCompile;
+import org.gradle.api.tasks.compile.CompileOptions;
+
+import java.io.File;
 
 /**
  * Task for kompilering av java-ws weblogic server implementasjon
@@ -25,89 +30,102 @@ import org.gradle.api.tasks.compile.CompileOptions
 class WeblogicWsCompileTask extends AbstractCompile implements WeblogicTaskInterface {
     protected static final Logger logger = Logging.getLogger(WeblogicWsCompileTask.class);
 
-    private WeblogicJaxWsCompiler compiler
+    private WeblogicJaxWsCompiler compiler = new WeblogicJaxWsCompiler();
     private final DefaultWeblogicCompileSpec spec = new DefaultWeblogicCompileSpec();
 
-    private FileCollection weblogicClasspath
+    private FileCollection weblogicClasspath;
 
-    File getGenDir() { return genDir ?: project.file("${project.buildDir}/weblogic/jwsc") }
-    File genDir
-
-    @OutputDirectory
-    File getClassesDir() { return classesDir ?: project.file("${destinationDir}/WEB-INF/classes") }
-    File classesDir  //for filer som skal på classpath
+    public File getGenDir() {
+        return genDir != null ? genDir : getProject().file("${project.buildDir}/weblogic/jwsc");
+    }
+    private File genDir;
 
     @OutputDirectory
-    File getGenSourcesDir() { genSourcesDir ?: project.file("gen/weblogic/jwsc") }
-    File genSourcesDir
+    public File getClassesDir() {
+        return classesDir != null ? classesDir : getProject().file("${destinationDir}/WEB-INF/classes");
+    }
+    private File classesDir;  //for filer som skal på classpath
+
+    @OutputDirectory
+    public File getGenSourcesDir() {
+        return genSourcesDir != null ? genSourcesDir : getProject().file("gen/weblogic/jwsc");
+    }
+    private File genSourcesDir;
 
 
 
     WeblogicWsCompileTask() {
-        logging.captureStandardOutput LogLevel.INFO
-        logging.captureStandardError LogLevel.DEBUG
-
-        compiler = new WeblogicJaxWsCompiler()
+        getLogging().captureStandardOutput(LogLevel.INFO);
+        getLogging().captureStandardError(LogLevel.DEBUG);
 
         //setting defaults for this compiler.
-        getOptions().setFork(true)
-        getOptions().setListFiles(true)
-        getOptions().setVerbose(logger.isDebugEnabled())
-        getOptions().setFailOnError(true) //defaults to true
+        getOptions().setFork(true);
+        getOptions().setListFiles(true);
+        getOptions().setVerbose(logger.isDebugEnabled());
+        getOptions().setFailOnError(true); //defaults to true
 
     }
 
     @TaskAction
     protected void compile() {
-        compiler.ant = getAnt()
-        compiler.baseDir = project.file('src')
-        compiler.warName = project.name + ".war"
-        compiler.fileResolver = ((ProjectInternal)getProject()).getFileResolver()
+        final Project project = getProject();
+        compiler.setAnt(project.getAnt());
+        compiler.setBaseDir(project.file("src"));
+        compiler.setWarName(project.getName() + ".war");
+        compiler.setFileResolver(((ProjectInternal) project).getFileResolver());
 
-        spec.weblogicClasspath = getWeblogicClasspath().files
-        spec.setTempDir(getTemporaryDir())
+        spec.setWeblogicClasspath(getWeblogicClasspath().getFiles());
+        spec.setTempDir(getTemporaryDir());
 
         spec.setDestinationDir(getGenDir());
         spec.setSource(getSource());
         spec.setClasspath(getClasspath());
 
 
-        project.delete(spec.getDestinationDir()) //no dirty files
+        project.delete(getGenDir()); //no dirty files
 
         WorkResult result = compiler.execute(spec);
         setDidWork(result.getDidWork());
 
 
         //extract application files
-        project.delete(getDestinationDir()) //no dirty files
-        project.copy {
-           into this.getDestinationDir()
-           from("${this.getGenDir()}/${project.name}.war") {
-               exclude 'WEB-INF/classes/**'
-               exclude 'WEB-INF/web.xml'
-               exclude '**/*.java'
-           }
-           includeEmptyDirs = false
-        }
+        project.delete(getDestinationDir()); //no dirty files
+
+        project.copy(new MethodClosure(this, "applicationFilesCopySpec"));
 
         //extract generated classes
-        project.delete(getClassesDir()) //no dirty files
-        project.copy {
-           into this.getClassesDir()
-           from("${getGenDir()}/${project.name}.war/WEB-INF/classes")
-        }
+        project.delete(getClassesDir()); //no dirty files
+        project.copy(new MethodClosure(this, "generatedClassesCopySpec"));
 
         //extract source files
-        project.delete(genSourcesDir) //no dirty files
-        project.copy {
-           into this.genSourcesDir
-           from("${this.getGenDir()}/${project.name}.war") {
-               exclude 'WEB-INF/**' //only java source
-           }
-        }
-
-
+        project.delete(getGenSourcesDir()); //no dirty files
+        project.copy(new MethodClosure(this, "generatedSourcesCopySpec"));
     }
+
+
+    private void applicationFilesCopySpec(CopySpec spec) {
+        spec.into(getDestinationDir());
+        spec.from(getGenDir().getPath() + "/" + getProject().getName() + ".war")
+                .exclude("WEB-INF/classes/**"
+                        , "WEB-INF/web.xml"
+                        , "**/*.java"
+                );
+        spec.setIncludeEmptyDirs(false);
+    }
+
+    private void generatedClassesCopySpec(CopySpec spec) {
+        spec.into(getClassesDir());
+        spec.from(getGenDir().getPath() + "/" + getProject().getName() + ".war/WEB-INF/classes")
+        ;
+    }
+
+    private void generatedSourcesCopySpec(CopySpec spec) {
+        spec.into(getClassesDir());
+        spec.from(getGenDir().getPath() + "/" + getProject().getName() + ".war/")
+                .exclude("WEB-INF/**") //no compiled class files etc...
+        ;
+    }
+
 
     /**
      * Returns the compilation options.
