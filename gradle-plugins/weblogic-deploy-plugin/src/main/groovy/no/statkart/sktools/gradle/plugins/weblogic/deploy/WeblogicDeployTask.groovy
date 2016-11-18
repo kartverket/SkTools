c95feb8858c9ecbe3a7c2ca5b335562a89944101
Leif Eirik Lislegård <lislei@statkart.no>
2016-11-18 17:47:11 +0100

    SKTOOLS-157: Deploy til weblogic bør feile ved manglende ear fil
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 75207]

--- gradle-plugins/weblogic-deploy-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/deploy/WeblogicDeployTask.groovy
+++ gradle-plugins/weblogic-deploy-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/deploy/WeblogicDeployTask.groovy
@@ -1,5 +1,6 @@
 package no.statkart.sktools.gradle.plugins.weblogic.deploy
 
+import org.gradle.api.file.FileCollection
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 import org.gradle.api.tasks.Input
@@ -32,6 +33,9 @@ class WeblogicDeployTask extends AbstractWeblogicDeployTask {
     @TaskAction
     void deploy() {
         logger.quiet("Deployment av ${getDeploymentName()} til Weblogic paa ${getUrl()}")
+
+        checkSourceExists()
+
 
         Map args = [
                 action: 'deploy',
@@ -68,4 +72,19 @@ class WeblogicDeployTask extends AbstractWeblogicDeployTask {
         return logger;
     }
 
+    void checkSourceExists() {
+        FileCollection fileCollection = project.files(getFile())
+        if (fileCollection.isEmpty()) {
+            logger.error ''
+            logger.error 'ERROR: Ingen fil aa deploye. \nHar du konfigurert opp denne tasken riktigt?'
+            logger.error ''
+            throw new RuntimeException("Source for task $name er ikke konfigurert!");
+        }
+        if (!fileCollection.singleFile.exists()) {
+            logger.error ''
+            logger.error 'ERROR: Ingen fil aa deploye. \nHar du husket assemble?'
+            logger.error ''
+            throw new FileNotFoundException(String.valueOf(getFile()));
+        }
+    }
 }
