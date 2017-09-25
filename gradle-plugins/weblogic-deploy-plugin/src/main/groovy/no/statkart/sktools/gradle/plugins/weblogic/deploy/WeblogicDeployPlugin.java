27858855a84fd94ba5747e7326c015977610fbc3
Leif Eirik Lislegård <lislei@statkart.no>
2017-09-25 13:17:24 +0100

    * skrevet om en klasse til java
    [git-p4: depot-paths = "//sktools/SKToolsKode/trunk/": change = 81196]

---
+++ gradle-plugins/weblogic-deploy-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/deploy/WeblogicDeployPlugin.java
@@ -0,0 +1,23 @@
+package no.statkart.sktools.gradle.plugins.weblogic.deploy;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.internal.project.ProjectInternal;
+
+/**
+ * Plugin for deployment-tasker til Weblogic Server.
+ *
+ * @author Tor Egil R. Strand
+ * @author Leif Lislegård
+ * @since 1.2
+ */
+public class WeblogicDeployPlugin implements Plugin<ProjectInternal> {
+    public final static String WEBLOGIC_DEPLOY_CONVENTION_NAME = "weblogicDeployConvention";
+
+    @Override
+    public void apply(ProjectInternal project) {
+
+        WeblogicDeployConvention convention = new WeblogicDeployConvention(project);
+        project.getConvention().getPlugins().put(WEBLOGIC_DEPLOY_CONVENTION_NAME, convention);
+
+    }
+}
