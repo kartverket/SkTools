40ecb8751675d206cd79b5d17c0e6018a881e9dd
Leif Eirik Lislegård <lislei@statkart.no>
2019-10-29 09:10:22 +0100

    SKTOOLS-196: fixed leaking file handle
    [git-p4: depot-paths = "//sktools/SKToolsKode/1.5/": change = 92646]

--- build-utils/wsdocgen/src/main/java/no/statkart/sktools/utils/wsdocgen/processor/WSDocProcessor.java
+++ build-utils/wsdocgen/src/main/java/no/statkart/sktools/utils/wsdocgen/processor/WSDocProcessor.java
@@ -211,9 +211,9 @@ public class WSDocProcessor extends AbstractProcessor {
         if (xsltFilePath != null) {
             final File xsltFile = new File(xsltFilePath);
             if (xsltFile.exists()) {
-                try {
+                try (OutputStream os = outputFile.openOutputStream()) {
                     final DOMSource in = new DOMSource(document);
-                    StreamResult out = new StreamResult(outputFile.openOutputStream());
+                    StreamResult out = new StreamResult(os);
                     StreamSource xsltStream = new StreamSource(xsltFile);
 
                     transform(in, out, xsltStream);
