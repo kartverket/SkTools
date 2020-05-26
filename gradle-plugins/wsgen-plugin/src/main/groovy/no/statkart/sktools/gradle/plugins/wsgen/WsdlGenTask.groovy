ae4aa0cef0968602d198f9f8482b30460e2c5012
Tor Egil Strand <strant@kartverket.no>
2020-05-26 10:15:50 +0200

    SKTOOLS-210: Kutter ut Ant som mellomledd for å få det til å virke med Gradle 6 og Java 11

--- gradle-plugins/wsgen-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/wsgen/WsdlGenTask.groovy
+++ gradle-plugins/wsgen-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/wsgen/WsdlGenTask.groovy
@@ -7,53 +7,42 @@ import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
 
-public class WsdlGenTask extends SourceTask {
+class WsdlGenTask extends SourceTask {
     @CompileClasspath
-    FileCollection classpath;
+    FileCollection classpath
 
     @CompileClasspath
-    FileCollection jaxwsClasspath;
+    FileCollection jaxwsClasspath
 
     @OutputDirectory
-    File destinationDir;
+    File destinationDir
 
     @TaskAction
     protected void genWsdl() {
-        ant.taskdef(name: 'wsgen', classname: 'com.sun.tools.ws.ant.WsGen', classpath: getJaxwsClasspath().getAsPath())
         getProject().delete(getDestinationDir())
         getProject().mkdir(getDestinationDir())
 
         def wsBeans = getSource().matching { include '**/*WSBean.class' }
-
-        def classpathRef = getPath().replace(':' as char, '_' as char)
-        createAntClassPath(getClasspath(), classpathRef)
+        def cp = getClasspath().asPath
 
         wsBeans.visit { FileVisitDetails details ->
             if (!details.directory) {
                 String path = details.getRelativePath()
                 String classname = path.substring(0, path.length() - 6) //removing '.class'
                     .replace('/' as char, '.' as char)
-                ant.wsgen(
-                        sei: classname,
-                        classpathref: classpathRef,
-                        destdir: getTemporaryDir(),
-                        resourcedestdir: getDestinationDir(),
-                        genwsdl: 'true',
-                        encoding: 'UTF-8',
-                        keep: 'true',
-                        xnocompile: 'true',
-                        fork: 'true',
-                )
-            }
-        }
-    }
 
-    private void createAntClassPath(Iterable classpath, String id) {
-        getLogger().debug('Defining Ant classpath id={}', id)
-        getAnt().path(id: id) {
-            classpath.each {
-                getLogger().debug('\t{} += {}', id, it)
-                pathelement(location: it)
+                getProject().javaexec {
+                    classpath = getJaxwsClasspath()
+                    main = 'com.sun.tools.ws.WsGen'
+                    args '-d', getTemporaryDir()
+                    args '-keep'
+                    args '-encoding', 'UTF-8'
+                    args '-classpath', cp
+                    args '-Xnocompile'
+                    args '-wsdl'
+                    args '-r', getDestinationDir()
+                    args classname
+                }
             }
         }
     }
