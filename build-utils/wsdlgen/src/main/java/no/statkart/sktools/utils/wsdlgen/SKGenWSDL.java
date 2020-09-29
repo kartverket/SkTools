f51e065a790ddcc5c7010a40f57982a7a6785f7c
Tor Egil Strand <strant@kartverket.no>
2020-09-29 13:31:18 +0200

    SKTOOLS-216: Raskere WSDL-generering ved å generere alle i én prosess, og nytt plugin som bruker dette verktøyet

---
+++ build-utils/wsdlgen/src/main/java/no/statkart/sktools/utils/wsdlgen/SKGenWSDL.java
@@ -0,0 +1,134 @@
+package no.statkart.sktools.utils.wsdlgen;
+
+import com.oracle.webservices.api.databinding.WSDLResolver;
+import com.sun.xml.ws.api.BindingID;
+import com.sun.xml.ws.api.databinding.DatabindingConfig;
+import com.sun.xml.ws.api.databinding.DatabindingFactory;
+import com.sun.xml.ws.api.databinding.WSDLGenInfo;
+import com.sun.xml.ws.binding.WebServiceFeatureList;
+import com.sun.xml.ws.db.DatabindingImpl;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ScanResult;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import javax.jws.WebService;
+import javax.xml.transform.Result;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.ws.Holder;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class SKGenWSDL {
+    public static void main(String[] args) throws IOException, ParseException {
+        Options options = new Options()
+            .addOption(
+                Option.builder("cp")
+                    .longOpt("classpath")
+                    .hasArg()
+                    .desc("Classpath for web services")
+                    .required()
+                    .build()
+            )
+            .addOption(
+                Option.builder("d")
+                    .longOpt("destination")
+                    .hasArg()
+                    .desc("Destination for WSDLs and schemas")
+                    .required()
+                    .build()
+            );
+
+        CommandLine commandLine = new DefaultParser()
+            .parse(options, args);
+
+        String classpath = commandLine.getOptionValue("cp");
+        Path destination = Paths.get(commandLine.getOptionValue("d"));
+
+        DatabindingFactory databindingFactory = DatabindingFactory.newInstance();
+
+        try (ScanResult scanResult = new ClassGraph()
+            .overrideClasspath(classpath)
+            .enableClassInfo()
+            .enableAnnotationInfo()
+            .enableExternalClasses()
+            .scan()) {
+            for (ClassInfo classInfo : scanResult.getClassesWithAnnotation(WebService.class.getName())) {
+                if (classInfo.isStandardClass()) {
+                    Class<?> wsClass = classInfo.loadClass();
+                    genWSDL(wsClass, destination, databindingFactory);
+                }
+            }
+        }
+    }
+
+    private static void genWSDL(Class<?> endpointClass, Path destinationDirectory, DatabindingFactory databindingFactory) throws IOException {
+        DatabindingConfig config = new DatabindingConfig();
+        config.setClassLoader(endpointClass.getClassLoader());
+        config.setEndpointClass(endpointClass);
+        config.setFeatures(new WebServiceFeatureList(endpointClass));
+        config.getMappingInfo().setBindingID(BindingID.parse(endpointClass));
+        DatabindingImpl runtime = (DatabindingImpl) databindingFactory.createRuntime(config);
+
+        ArrayList<OutputStream> streams = new ArrayList<>();
+        WSDLGenInfo wsdlGenInfo = new WSDLGenInfo();
+        wsdlGenInfo.setWsdlResolver(new WSDLResolver() {
+            @Override
+            public Result getWSDL(String suggestedFilename) {
+                try {
+                    Path path = destinationDirectory.resolve(suggestedFilename);
+                    OutputStream outputStream = Files.newOutputStream(path);
+                    streams.add(outputStream);
+                    StreamResult result = new StreamResult(outputStream);
+                    result.setSystemId(path.toFile());
+                    return result;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            public Result getAbstractWSDL(Holder<String> filename) {
+                try {
+                    Path path = destinationDirectory.resolve(filename.value);
+                    OutputStream outputStream = Files.newOutputStream(path);
+                    streams.add(outputStream);
+                    StreamResult result = new StreamResult(outputStream);
+                    result.setSystemId(path.toFile());
+                    return result;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            public Result getSchemaOutput(String namespace, Holder<String> filename) {
+                try {
+                    Path path = destinationDirectory.resolve(filename.value);
+                    OutputStream outputStream = Files.newOutputStream(path);
+                    streams.add(outputStream);
+                    StreamResult result = new StreamResult(outputStream);
+                    result.setSystemId(path.toFile());
+                    return result;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        });
+        runtime.generateWSDL(wsdlGenInfo);
+        for (OutputStream stream : streams) {
+            stream.close();
+        }
+    }
+}
