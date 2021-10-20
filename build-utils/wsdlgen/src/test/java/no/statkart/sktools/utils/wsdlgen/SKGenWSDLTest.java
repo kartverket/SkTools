e48fd98ecc2d7d3e822d3e3154ec5316d144e7a4
Leif Lislegard <lislei@kartverket.no>
2021-10-20 10:44:14 +0200

    SKTOOLS-239: Lagt til håndtering av lang classpath for WsdlGenTask
    Definerer classpath for scanning til verktøy som environment variabel.

--- build-utils/wsdlgen/src/test/java/no/statkart/sktools/utils/wsdlgen/SKGenWSDLTest.java
+++ build-utils/wsdlgen/src/test/java/no/statkart/sktools/utils/wsdlgen/SKGenWSDLTest.java
@@ -5,8 +5,14 @@ import org.testng.annotations.Test;
 
 public class SKGenWSDLTest {
     @Test
-    public void paakrevdeParametre() {
-        Assertions.assertThatThrownBy(() -> SKGenWSDL.main(new String[0]))
-            .hasMessage("Missing required options: cp, d");
+    public void destinationErPaakrevd() {
+        Assertions.assertThatThrownBy(() -> SKGenWSDL.main("foo"))
+            .hasMessage("Missing required option: d");
+    }
+
+    @Test
+    public void classpathErPaakrevd() {
+        Assertions.assertThatThrownBy(() -> SKGenWSDL.main("-d bar"))
+            .hasMessage("Classpath not set. Use parameter -cp <value> or environment variable WEB_SERVICE_CLASSPATH.");
     }
 }
