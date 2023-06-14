f7c73dc5e882e64fd18c61ce923947d23f4ab1ec
Leif Lislegard <lislei@kartverket.no>
2023-06-14 12:57:57 +0200

    Fjerner WeblogicBasePlugin
    TH-517

--- gradle-plugins/weblogic-plugin/src/main/groovy/no/statkart/sktools/gradle/plugins/weblogic/compile/Compiler.java
+++
@@ -1,7 +0,0 @@
-package no.statkart.sktools.gradle.plugins.weblogic.compile;
-
-import org.gradle.api.tasks.WorkResult;
-
-public interface Compiler<T extends WeblogicCompileSpec> {
-    WorkResult execute(T spec);
-}
